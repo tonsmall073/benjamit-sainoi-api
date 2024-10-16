9ef88cab5ac3181456477c35afb90756878b48c8
tonsmall073 <tonsmall073@gmail.com>
2024-10-17 01:11:10 +0700

    add function createEnumCondition and dropAllEnumTypes for use migrate

--- db/migrate.go
+++ db/migrate.go
@@ -27,7 +27,7 @@ func migrateBenjamitDatabase() {
 	}
 
 	errs := createEnums(db)
-	for err := range errs {
+	for _, err := range errs {
 		log.Printf("[ERROR] failed to create enum type : %v\n", err)
 		return
 	}
@@ -49,8 +49,6 @@ func migrateBenjamitDatabase() {
 }
 
 func createEnums(db *gorm.DB) []error {
-	var errors []error
-
 	enumDefinitions := map[string][]string{
 		"role_enum": {
 			string(models.USER),
@@ -63,15 +61,7 @@ func createEnums(db *gorm.DB) []error {
 		},
 	}
 
-	for name, values := range enumDefinitions {
-		joinedValues := strings.Join(values, "', '")
-		query := fmt.Sprintf("CREATE TYPE %s AS ENUM ('%s');", name, joinedValues)
-		if err := db.Exec(query).Error; err != nil {
-			errors = append(errors, err)
-		}
-	}
-
-	return errors
+	return createEnumCondition(db, enumDefinitions)
 }
 
 func createUUIDExtension(db *gorm.DB) error {
@@ -87,6 +77,39 @@ func setTimeZone(db *gorm.DB) error {
 	err := db.Exec("SET timezone = '" + defaultStr + "';").Error
 	var timezone string
 	db.Raw("SHOW TIMEZONE").Scan(&timezone)
-	log.Println("Current Timezone:", timezone)
+	log.Println("[INFO] current timezone:", timezone)
 	return err
+}
+
+func createEnumCondition(db *gorm.DB, enumDefinitions map[string][]string) []error {
+	var errors []error
+	for name, values := range enumDefinitions {
+		var exists bool
+		if err := db.Raw("SELECT EXISTS (SELECT 1 FROM pg_type WHERE typname = ?)", name).Scan(&exists).Error; err != nil {
+			errors = append(errors, err)
+		}
+
+		if exists {
+			for _, value := range values {
+				var eumExists bool
+				query := fmt.Sprintf("SELECT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = '%s' AND enumtypid = (SELECT oid FROM pg_type WHERE typname = '%s'));", value, name)
+				if err := db.Raw(query).Scan(&eumExists).Error; err != nil {
+					errors = append(errors, err)
+					eumExists = false
+				}
+				if !eumExists {
+					if err := db.Exec(fmt.Sprintf("ALTER TYPE %s ADD VALUE '%s';", name, value)).Error; err != nil {
+						errors = append(errors, err)
+					}
+				}
+			}
+		} else {
+			joinedValues := strings.Join(values, "', '")
+			query := fmt.Sprintf("CREATE TYPE %s AS ENUM ('%s');", name, joinedValues)
+			if err := db.Exec(query).Error; err != nil {
+				errors = append(errors, err)
+			}
+		}
+	}
+	return errors
 }
--- utils/dropAllTables.go
+++ utils/dropAllTables.go
@@ -9,15 +9,40 @@ import (
 func DropAllTables(db *gorm.DB) error {
 	tables, err := db.Migrator().GetTables()
 	if err != nil {
-		log.Fatal("failed to drop all tables:", err)
+		log.Fatal("[ERROR] failed to drop all tables:", err)
 		return err
 	}
 	for _, table := range tables {
 		if err := db.Migrator().DropTable(table); err != nil {
-			log.Fatal("failed to drop all tables:", err)
+			log.Fatal("[ERROR] failed to drop all tables:", err)
 			return err
 		}
 	}
-	log.Println("Drop all tables Complete.")
+	if err := dropAllEnumTypes(db); err != nil {
+		log.Fatal("[ERROR] failed to drop all enum types:", err)
+		return err
+	}
+	log.Println("[INFO] drop all tables complete.")
+	return nil
+}
+
+func dropAllEnumTypes(db *gorm.DB) error {
+	var enumTypes []string
+
+	// ค้นหา enum types ทั้งหมด
+	if err := db.Raw(`
+		SELECT typname 
+		FROM pg_type 
+		WHERE typtype = 'e';`).Scan(&enumTypes).Error; err != nil {
+		return err
+	}
+
+	// ลบ enum types ทั้งหมด
+	for _, enumType := range enumTypes {
+		if err := db.Exec("DROP TYPE IF EXISTS " + enumType).Error; err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
