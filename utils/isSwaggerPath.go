27fd0e1e114110a03086d01dfefe81cb674cdc0d
tonsmall073 <tonsmall073@gmail.com>
2024-10-20 16:02:22 +0700

    Update isSwaggerPath.go

--- utils/isSwaggerPath.go
+++ utils/isSwaggerPath.go
@@ -5,7 +5,7 @@ import "strings"
 func IsSwaggerPath(path string) bool {
 	swaggerPaths := []string{"/api/docs", "/swagger", "/swagger/doc.json"}
 	for _, swaggerPath := range swaggerPaths {
-		if path == swaggerPath || strings.HasPrefix(path, swaggerPath) {
+		if strings.HasPrefix(path, swaggerPath) {
 			return true
 		}
 	}
