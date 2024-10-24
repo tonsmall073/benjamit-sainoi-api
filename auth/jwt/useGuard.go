1b2020d1bdfe0dce488a1efa7f3ee4413265a61c
tonsmall073 <tonsmall073@gmail.com>
2024-10-24 15:19:36 +0700

    remove chan channel

--- auth/jwt/useGuard.go
+++ auth/jwt/useGuard.go
@@ -3,121 +3,51 @@ package jwt
 import (
 	"bjm/utils"
 	"strings"
-	"sync"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func UseUserGuard(c *fiber.Ctx) error {
-	errorChan := make(chan utils.ErrorResponseModel)
-	resultChan := make(chan error)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		authHeader := c.Get("Authorization")
-		if authHeader == "" {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		if !strings.HasPrefix(authHeader, "Bearer ") {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		token := strings.TrimPrefix(authHeader, "Bearer ")
-		res, err := VerifyToken(token)
-		if err != nil {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		if res["role"].(string) != "USER" && res["role"].(string) != "ADMIN" {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[403],
-				StatusCode:  403,
-			}
-			return
-		}
-		resultChan <- c.Next()
-	}()
-
-	go func() {
-		wg.Wait()
-		close(errorChan)
-		close(resultChan)
-	}()
-
-	select {
-	case err := <-errorChan:
-		return utils.FiberResponseErrorJson(c, err.MessageDesc, err.StatusCode)
-	case result := <-resultChan:
-		return result
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
 	}
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	res, err := VerifyToken(token)
+	if err != nil {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
+	}
+
+	role := res["role"].(string)
+	if role != "USER" && role != "ADMIN" {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[403], 403)
+	}
+
+	return c.Next()
 }
 
 func UseAdminGuard(c *fiber.Ctx) error {
-	errorChan := make(chan utils.ErrorResponseModel)
-	resultChan := make(chan error)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		authHeader := c.Get("Authorization")
-		if authHeader == "" {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		if !strings.HasPrefix(authHeader, "Bearer ") {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		token := strings.TrimPrefix(authHeader, "Bearer ")
-		res, err := VerifyToken(token)
-		if err != nil {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[401],
-				StatusCode:  401,
-			}
-			return
-		}
-		if res["role"].(string) != "ADMIN" {
-			errorChan <- utils.ErrorResponseModel{
-				MessageDesc: utils.HttpStatusCodes[403],
-				StatusCode:  403,
-			}
-			return
-		}
-		resultChan <- c.Next()
-	}()
-
-	go func() {
-		wg.Wait()
-		close(errorChan)
-		close(resultChan)
-	}()
-
-	select {
-	case err := <-errorChan:
-		return utils.FiberResponseErrorJson(c, err.MessageDesc, err.StatusCode)
-	case result := <-resultChan:
-		return result
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
 	}
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	res, err := VerifyToken(token)
+	if err != nil {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[401], 401)
+	}
+
+	if res["role"].(string) != "ADMIN" {
+		return utils.FiberResponseErrorJson(c, utils.HttpStatusCodes[403], 403)
+	}
+
+	return c.Next()
 }
